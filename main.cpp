61ac2b20e8d1916ed57bb140c28e70bcce46d1d2
Aly Khreis <akakhreis@gmail.com>
2026-05-13 21:02:15 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<hash_table.h>
+using std::cout;// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    HashTable* ht = ht_new(50);
+
+    ht_insert(ht, "apple", "5");
+    ht_insert(ht, "banana", "3");
+    ht_insert(ht, "cherry", "8");
+
+    cout << ht_search(ht, "apple") << "\n";   // 5
+    cout << ht_search(ht, "banana") << "\n";  // 3
+    cout << ht_search(ht, "cherry") << "\n";  // 8
+
+    // test update
+    ht_insert(ht, "apple", "99");
+    cout << ht_search(ht, "apple") << "\n";   // 99
+
+    // test delete
+    ht_delete("banana", ht);
+    cout << ht_search(ht, "banana") << "\n";  // empty
+
+    ht_delete(ht);
+}
